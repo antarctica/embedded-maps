2aabebe0658ce680e736f6426bc08a336c65f8bb
Jonny Dawe <jonwe@bas.ac.uk>
2025-03-28 14:16:31 +0000

    turn off updating nertwork capture for test

--- e2e/specs/MapViewParams.spec.ts
+++ e2e/specs/MapViewParams.spec.ts
@@ -60,7 +60,7 @@ test.describe.parallel('Map View Parameters', () => {
     test.beforeEach(async ({ page }) => {
       await page.routeFromHAR(`e2e/hars/map-view-params/asset-id/asset-id.har`, {
         url: '**/tPxy1hrFDhJfZ0Mf/arcgis/rest/services/ats_latest_assets_position/FeatureServer/0/*',
-        update: true,
+        update: false,
       });
       await page.goto(`/?asset-id=01JDRYA29AR6PFGXVCZ40V8C74&zoom=8`);
       await waitForMapReady(page);
